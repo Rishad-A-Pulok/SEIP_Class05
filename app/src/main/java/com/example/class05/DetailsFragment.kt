5c1066f56a4cdf8113a830841579ec2e01725fe3
Rishad Amin Pulok <rishad15-2117@diu.edu.bd>
2022-03-31 10:44:17 +0600

    data binding library used

--- app/src/main/java/com/example/class05/DetailsFragment.kt
+++ app/src/main/java/com/example/class05/DetailsFragment.kt
@@ -6,36 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.example.class05.databinding.FragmentDetailsBinding
+import com.example.class05.databinding.FragmentHomeBinding
 import org.w3c.dom.Text
 
 class DetailsFragment : Fragment() {
-    private lateinit var fNameTV: TextView
-    private lateinit var lNameTV: TextView
-    private lateinit var emailTV: TextView
-    private lateinit var phoneTV: TextView
+    private lateinit var binding: FragmentDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        fNameTV = view.findViewById(R.id.fNameTV)
-        lNameTV = view.findViewById(R.id.lNameTV)
-        emailTV = view.findViewById(R.id.emailTV)
-        phoneTV = view.findViewById(R.id.phoneTV)
+        binding = FragmentDetailsBinding.inflate(inflater,container,false)
         val fName = arguments?.getString("first_name")
         val lName = arguments?.getString("last_name")
         val email = arguments?.getString("email")
         val phone = arguments?.getString("phone")
-
-        fNameTV.text = fName.toString()
-        lNameTV.text = lName.toString()
-        emailTV.text = email.toString()
-        phoneTV.text = phone.toString()
+        binding.fNameTV.text = fName.toString()
+        binding.lNameTV.text = lName.toString()
+        binding.emailTV.text = email.toString()
+        binding.phoneTV.text = phone.toString()
+        return binding.root
     }
 }
--- app/src/main/java/com/example/class05/HomeFragment.kt
+++ app/src/main/java/com/example/class05/HomeFragment.kt
@@ -8,34 +8,22 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.navigation.fragment.findNavController
+import com.example.class05.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    private lateinit var fNmaeET: EditText
-    private lateinit var lNameET: EditText
-    private lateinit var emailET: EditText
-    private lateinit var phoneET: EditText
-    private lateinit var regBtn: Button
+    private lateinit var binding: FragmentHomeBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        fNmaeET = view.findViewById(R.id.fNameET)
-        lNameET = view.findViewById(R.id.lNameET)
-        emailET = view.findViewById(R.id.emailET)
-        phoneET = view.findViewById(R.id.phoneET)
-        regBtn = view.findViewById(R.id.regBtn)
-        regBtn.setOnClickListener {
-            val fName = fNmaeET.text.toString()
-            val lName = lNameET.text.toString()
-            val email = emailET.text.toString()
-            val phone = phoneET.text.toString()
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.regBtn.setOnClickListener {
+            val fName = binding.fNameET.text.toString()
+            val lName = binding.lNameET.text.toString()
+            val email = binding.emailET.text.toString()
+            val phone = binding.phoneET.text.toString()
 
             val bundle = Bundle().apply {
                 putString("first_name",fName)
@@ -44,7 +32,7 @@ class HomeFragment : Fragment() {
                 putString("phone",phone)
             }
             findNavController().navigate(R.id.action_homeFragment2_to_detailsFragment2,bundle)
-
         }
+        return binding.root
     }
 }
